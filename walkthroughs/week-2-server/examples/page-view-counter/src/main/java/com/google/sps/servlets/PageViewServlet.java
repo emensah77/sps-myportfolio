0fbf51218a3cfffc8137f7ba404a441abb9023e6
Ebenezer <emensah.em77@gmail.com>
2021-02-25 03:51:46 +0000

    Updates to the servlet class

--- walkthroughs/week-2-server/examples/page-view-counter/src/main/java/com/google/sps/servlets/PageViewServlet.java
+++ walkthroughs/week-2-server/examples/page-view-counter/src/main/java/com/google/sps/servlets/PageViewServlet.java
@@ -29,6 +29,7 @@ public class PageViewServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     pageViews++;
+    
 
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Page Views</h1>");
