353c732ca78a95dac14588b0e5a5238c5a71d737
Ebenezer <emensah.em77@gmail.com>
2021-03-09 00:02:05 +0000

    C.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -32,7 +32,8 @@ async function getServerMessages() {
     arr.push(messages[i]);
     }
   const messageListElement = document.getElementById('message-container');
-  messageListElement.innerText = arr[Math.floor(Math.random() * arr.length)];
+  messageListElement.innerText = arr[Math.floor( Math.floor(Math.random()*arr.length))];
+  //arr[Math.floor(Math.random() * arr.length)];
      
 }
 
