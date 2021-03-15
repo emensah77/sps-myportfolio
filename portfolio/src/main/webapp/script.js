611af43035a1146ec0383e4cfcf6c78822874250
Ebenezer <emensah.em77@gmail.com>
2021-03-15 22:31:26 +0000

    Update script.js and index.html to load a welcome screen.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Splash screen
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.splash-header');
+let logoSpan = document.querySelectorAll('.splash');
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+
+                }, (idx + 1) * 50)
+            })
+        }, 2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh'
+        }, 2300)
+
+    })
+
+
+
+})
 
 async function showServerMessage() {
   const responseFromServer = await fetch('/new');
